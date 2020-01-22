c4223be1fd652734665c1bab8a9663d8d93b1ccc
yydia <bcwz1@outlook.com>
2020-01-22 15:14:32 +0800

    update ipa/363/index.html

--- ipa/363/index.php
+++ ipa/363/index.php
@@ -1,3 +1,17 @@
-<?php
-header("HTTP/1.1 301 Moved Permanently"); header("Location: http://152.136.120.155/tz/lz.php?url=https://www.lanzous.com/i8sr7af&type=down"); exit();
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Google</title>
+<style>
+body {
+width: 35em;
+margin: 0 auto;
+font-family: Tahoma, Verdana, Arial, sans-serif;
+}
+</style>
+<script>window.location.href='https://www.google.com';</script>
+</head>
+<body>
+ 
+</body>
+</html>
